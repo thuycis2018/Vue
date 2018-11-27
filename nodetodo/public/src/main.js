1af92808b52cf303f2e2ce9d1428425961577f70
Adrian Giurumescu <thuycis@gmail.com>
2018-11-26 17:04:10 -0800

    changed to lowercase

--- nodetodo/public/src/main.js
+++ nodetodo/public/src/main.js
@@ -1,14 +1,14 @@
-//public/main.js
+//public/src/main.js
 'use strict'
 import Vue from 'vue'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import App from './components/app.vue'
+import app from './components/app.vue'
 
 new Vue({
   el: 'app',
   created: function () {
     console.log('root instance was created')
   },
-  components: {App},
+  components: {app},
   methods: {}
 })
